fe73339f2033d2e0341f47f2cb9e84178a882493
Lesley <leskimuti@gmail.com>
2022-07-19 10:49:00 +0300

    Trying to use axios to get advices

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { useEffect } from "react";
+import axios from "axios";
+import Footer from "./components/Footer";
 import dice from "./images/icon-dice.svg";
 import dividermobile from "./images/pattern-divider-mobile.svg";
 import divider from "./images/pattern-divider-desktop.svg";
 
 export default function App() {
+  // Changing the divider image as per teh screen width dynamically
   const [width, setWidth] = React.useState(window.innerWidth);
   const updateDimensions = () => {
     setWidth(window.innerWidth);
   };
+
+  // Get Advices
+
+  let url = "https://api.adviceslip.com/advice";
+
+  const [advice, setAdvice] = React.useState({});
+
+  const fetchData = async () => {
+    const response = await axios.get(url).then((res) => {
+      console.log(res.data.slip);
+      setAdvice(res.data.slip);
+      return res.data.slip;
+    });
+  };
+
   useEffect(() => {
     window.addEventListener("resize", updateDimensions);
-    // return () => window.removeEventListener("resize", updateDimensions);
+    setAdvice(fetchData());
+    console.log(advice);
   }, []);
-  console.log(width);
+
+  const getAdvice = () => {
+    fetchData();
+  };
+
   return (
     <main className="container">
-      <h1 className="main--title">Advice #117</h1>
-      <blockquote>
-        "It is easy to sit up and take notice whats hard is to bllahblaah blaah"
-      </blockquote>
+      <h1 className="main--title">Advice #{advice.id}</h1>
+      {/* <h1 className="main--title">Advice #111</h1> */}
+      <blockquote>{advice.advice}</blockquote>
+      {/* <blockquote>"Hey brother"</blockquote> */}
       <div className="divider--img">
         <img src={width >= 580 ? divider : dividermobile} alt="dividerline" />
       </div>
-      <button className="btn--advice">
+      <button onClick={getAdvice} className="btn--advice">
         <img src={dice} alt="dice" />
       </button>
     </main>
