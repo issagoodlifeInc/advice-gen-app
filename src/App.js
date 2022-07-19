c0ac1675a9cad01ccb0d552a3698749db21ffeb9
Lesley <leskimuti@gmail.com>
2022-07-19 18:36:32 +0300

    Fixing axios issues and adding Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import dice from "./images/icon-dice.svg";
 import dividermobile from "./images/pattern-divider-mobile.svg";
 import divider from "./images/pattern-divider-desktop.svg";
@@ -19,36 +19,39 @@ export default function App() {
   const [advice, setAdvice] = React.useState({});
 
   const fetchData = async () => {
-    const response = await axios.get(url).then((res) => {
+    await axios.get(url).then((res) => {
       console.log(res.data.slip);
       setAdvice(res.data.slip);
       return res.data.slip;
     });
   };
 
+  // let initAdvice = fetchData();
+  // console.log(initAdvice);
+
   useEffect(() => {
     window.addEventListener("resize", updateDimensions);
     setAdvice(fetchData());
   }, []);
 
-  console.log(advice);
-
   const getAdvice = () => {
     fetchData();
+    console.log(advice);
   };
 
   return (
-    <main className="container">
-      <h1 className="main--title">Advice #{advice.id}</h1>
-      {/* <h1 className="main--title">Advice #111</h1> */}
-      <blockquote>{advice.advice}</blockquote>
-      {/* <blockquote>"Hey brother"</blockquote> */}
-      <div className="divider--img">
-        <img src={width >= 580 ? divider : dividermobile} alt="dividerline" />
-      </div>
-      <button onClick={getAdvice} className="btn--advice">
-        <img src={dice} alt="dice" />
-      </button>
-    </main>
+    <>
+      <main className="container">
+        <h1 className="main--title">Advice #{advice.id}</h1>
+        <blockquote>{advice.advice && `" ${advice.advice} "`}</blockquote>
+        <div className="divider--img">
+          <img src={width >= 580 ? divider : dividermobile} alt="dividerline" />
+        </div>
+        <button onClick={getAdvice} className="btn--advice">
+          <img src={dice} alt="dice" />
+        </button>
+      </main>
+      <Footer />
+    </>
   );
 }
